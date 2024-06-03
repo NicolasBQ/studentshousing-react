b9d8d186cf1bfe7b78f411b05a537e0850a9533c
Nicolas Baracaldo <nicolasbquintero@gmail.com>
2024-06-02 21:48:16 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Alert } from "@material-tailwind/react"
+
+function App() {
+  return (
+    <>
+      <Alert>
+        A simple alert
+      </Alert>
+    </>
+  )
+}
+
+export default App
